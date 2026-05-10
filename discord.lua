10cdc93c4a350dc1dc75f6d985efd60dea091294
zyqunix <fabiovanek2008@gmail.com>
2026-05-10 18:23:27 +0200

    debug

--- discord.lua
+++ discord.lua
@@ -95,6 +95,8 @@ while true do
     if file then
       local serverIndex = file:read("*line")
       local messageText = file:read("*all")
+      print(serverIndex)
+      print(messageText)
       file:close()
       
       if serverIndex and #messageText > 0 then
@@ -103,6 +105,7 @@ while true do
         local servers = dofile("/home/servers.lua")
         if servers[serverIndex] then
           local url = servers[serverIndex].value
+          print("sending to "..url)
           
           local contents = {
             content = messageText,
