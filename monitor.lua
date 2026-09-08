3cc4cbf715573e82ef14d3c52158baf92712e250
zyqunix <e@fbi.one>
2026-09-08 21:00:46 +0200

    fixes

--- monitor.lua
+++ monitor.lua
@@ -1,13 +1,20 @@
 local shell = require("shell")
 local c = require("component")
 
-local fission = c.nc_fission_reactor
-local fusion = c.nc_fusion_reactor
+local has_fission = false
+local has_fusion = false
+
+for address, component_type in c.list() do
+  if component_type == "nc_fission_reactor" then
+    has_fission = true
+  elseif component_type == "nc_fusion_reactor" then
+    has_fusion = true
+  end
+end
 
 while true do
-  if fission then shell.execute("./fission_reactor.lua") end
-  if fusion then shell.execute("./fusion_reactor.lua") end
+  if has_fission then shell.execute("./fission_reactor.lua") end
+  if has_fusion then shell.execute("./fusion_reactor.lua") end
   if fusion or fission then shell.execute("./discord.lua") end
-  
   os.sleep(300)
 end
