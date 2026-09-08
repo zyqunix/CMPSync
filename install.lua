f12529f76d99165ba3ee960317671f47827f1270
zyqunix <e@fbi.one>
2026-09-08 20:50:38 +0200

    install script

---
+++ install.lua
@@ -0,0 +1,28 @@
+local internet = require("internet")
+local fs = require("filesystem")
+local shell = require("shell")
+
+local files = {
+  "discord.lua",
+  "fission_reactor.lua",
+  "fusion_reactor.lua",
+  "monitor.lua",
+  "servers.lua"
+}
+
+local base_url = "https://raw.githubusercontent.com/zyqunix/CMPSync/refs/heads/master/"
+
+for _, file in ipairs(files) do
+  print("downloading " .. file)
+  local response = internet.request(base_url .. file)
+  local content = ""
+  for chunk in response do
+    content = content .. chunk
+  end
+  local handle = io.open(file, "w")
+  handle:write(content)
+  handle:close()
+  print("downloaded" .. file)
+end
+
+print("all downloads successful")
